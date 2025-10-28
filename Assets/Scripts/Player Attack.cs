642284f6f42543726682bc2cd9180454abda4d7a
Sage9 <olayiwolaladipo@gmail.com>
2025-10-28 17:51:08 +0100

    stuff

---
+++ Assets/Scripts/Player Attack.cs
@@ -0,0 +1,11 @@
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAttack : MonoBehaviour
+{
+   List<GameObject> HitObjects = new List<GameObject>();
+   [SerializeField] private Collider swordCollider;
+
+   
+}
